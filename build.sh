a2ed386444f85f788764042f0214d466c6be3f37
Shuo Chen <chenshuo@chenshuo.com>
2014-08-19 12:21:08 +0800

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
+LLVM_PATH=$HOME/llvm-3.4.2.src
+CPP_ARGS="-I $LLVM_PATH/tools/clang/include/ \
+  -I $LLVM_PATH/build/tools/clang/include/ \
+  -I $LLVM_PATH/include/ \
+  -I $LLVM_PATH/build/include/ \
+  -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS"
 
-g++ -g -Wall plugin.cc   -I /home/schen/llvm-3.4.2.src/tools/clang/include/ -I /home/schen/llvm-3.4.2.src/build/tools/clang/include/ -I /home/schen/llvm-3.4.2.src/include/ -I /home/schen/llvm-3.4.2.src/build/include/ -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS -c -fpic && gcc -shared -o indexer.so indexer.o
+set -x
 
-g++ -Wall -g  indexer.cc -I /home/schen/llvm-3.4.2.src/tools/clang/include/ -I /home/schen/llvm-3.4.2.src/build/tools/clang/include/ -I /home/schen/llvm-3.4.2.src/include/ -I /home/schen/llvm-3.4.2.src/build/include/ -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS /home/schen/llvm-3.4.2.src/build/lib/libclang.so -fno-rtti
+g++ -c -g -Wall plugin.cc $CPP_ARGS -fpic && gcc -shared -o indexer.so indexer.o
+
+g++ -fno-rtti -g -Wall indexer.cc $CPP_ARGS \
+  -L $LLVM_PATH/build/lib \
+  -lclangTooling -lclangFrontend -lclangSerialization \
+  -lclangDriver -lclangSema -lclangAnalysis -lclangEdit -lclangAST \
+  -lclangParse -lclangSema -lclangLex -lclangBasic \
+  -lLLVMBitReader -lLLVMMCParser -lLLVMMC -lLLVMTransformUtils \
+  -lLLVMOption -lLLVMCore -lLLVMSupport \
+  -ldl -ltinfo -lpthread
 
